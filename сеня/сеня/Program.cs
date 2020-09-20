243aca57b721060a1087ebd8d7fef71b2a4b28c3
ArsikUn <lazar10011@gmail.com>
2020-09-20 18:56:52 +0300

    я сломался

---
+++ сеня/сеня/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace сеня
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
